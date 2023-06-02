d1ddde71aabbbb6254310ef20695206be0571eb7
Aijazmakerb <72906651+Aijazmakerb@users.noreply.github.com>
2023-06-02 14:09:21 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -79,7 +79,7 @@ $json = json_decode($json, true);
             title: "<?=$json['title']?>",
             description: "You're Watching:",
             image: "<?=$json['image']?>",
-            sources: [{"file": "https://cors.zimjs.com/<?=$episodeLink?>"}],
+            sources: [{"file": "https://web-production-e5d2.up.railway.app/<?=$episodeLink?>"}],
             autostart: false,
         }],
         advertising: {
