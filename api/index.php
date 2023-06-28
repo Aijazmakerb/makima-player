43c6149b811ff1afd953b58a847464bda39e2a0d
Aijazmakerb <72906651+Aijazmakerb@users.noreply.github.com>
2023-06-28 14:13:37 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -18,8 +18,15 @@ $json = json_decode($json, true);
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://player.anikatsu.me/style.css">
     <meta name="robots" content="noindex, nofollow" />
-    <script src="https://code.jquery.com/jquery-3.6.3.min.js"
-        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script>
+      function preventFullscreenDoubleClick(event) {
+        // Prevent double-click event from triggering fullscreen mode
+        if (event.detail === 2) {
+          event.preventDefault();
+        }
+      }
+    </script>
 </head>
 <body>
     <style>
@@ -60,7 +67,7 @@ $json = json_decode($json, true);
         <div id="skipIntro"></div>
 
     </div>
-    <script src='https://player.anikatsu.me/jw.js?v=0.0002'></script>
+    <script src='./player.js'></script>
     <script>
         const playerInstance = jwplayer("player").setup({
         controls: true,
